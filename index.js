c06f880c09cc297d5adcfd5ccaa50e6234d9dee4
Nico <141285419+red4cted25@users.noreply.github.com>
2025-03-25 15:20:10 -0700

    Create index.ts
    Creates index.ts and began working with typescript

---
+++ index.js
@@ -0,0 +1,6 @@
+var firstName = "Nico";
+var coolio = true;
+var age = 17;
+var greeting = "Welcome to Jurassic Park";
+// age = "Month"
+firstName = "Angie";
---
+++ index.ts
@@ -0,0 +1,8 @@
+var firstName : string = "Nico";
+var coolio : boolean = true;
+var age : number = 17;
+
+let greeting = "Welcome to Jurassic Park";
+
+// age = "Month"
+firstName = "Angie"
