3fe83279254e5e5d8e5a8a77f5722ecfd4e56436
Nico <141285419+red4cted25@users.noreply.github.com>
2025-04-08 15:19:14 -0700

    Learned watching and functions in TS
    Created an add function in TS and learned to watch TS files

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+"use strict";
 var firstName = "Nico";
 var coolio = true;
 var age = 17;
-var greeting = "Welcome to Jurassic Park";
+let greeting = "Welcome to Jurassic Park";
 // age = "Month"
 firstName = "Angie";
+function add(n1, n2) {
+    return n1 + n2;
+}
+console.log(add(152689, 524189));
--- index.ts
+++ index.ts
@@ -5,4 +5,10 @@ var age : number = 17;
 let greeting = "Welcome to Jurassic Park";
 
 // age = "Month"
-firstName = "Angie"
+firstName = "Angie";
+
+function add(n1:number, n2:number) {
+    return n1 + n2;
+}
+
+console.log(add(152689, 524189))
