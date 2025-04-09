7c0154002016ef653e4f77311e15236d69aeb0ea
Nico <141285419+red4cted25@users.noreply.github.com>
2025-04-09 15:12:39 -0700

    Learned functions, other types
    Learned more about functions, arrow functions, enum, and other types in TS.

--- index.js
+++ index.js
@@ -9,3 +9,27 @@ function add(n1, n2) {
     return n1 + n2;
 }
 console.log(add(152689, 524189));
+function printResult(num) {
+    console.log('Result: ' + num);
+    return num;
+}
+const addArrow = (n1, n2) => n1 + n2;
+const printResultArrow = (num) => {
+    console.log('Result: ' + num);
+    // return num
+};
+let person;
+let product;
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "ADMIN";
+    Role[Role["READ_ONLY"] = 0] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 100] = "AUTHOR";
+})(Role || (Role = {}));
+let answer;
+function greet(user) {
+    console.log("Hi, I am " + user.name);
+}
+function isOlder(user, checkAge) {
+    return checkAge > user.age;
+}
--- index.ts
+++ index.ts
@@ -12,3 +12,55 @@ function add(n1:number, n2:number) {
 }
 
 console.log(add(152689, 524189))
+
+function printResult(num: number) : number {
+	console.log('Result: ' + num);
+	return num
+}
+
+const addArrow = (n1: number, n2: number) => n1 + n2;
+
+const printResultArrow = (num:number):void => {
+    console.log('Result: ' + num);
+    // return num
+}
+
+let person: {
+	name: string;
+	age: number;
+	hobbies: string[];
+	role: [number, string];
+} 
+
+let product: {
+	id: string;
+	price: number;
+	tags: string[];
+	details: {
+		title: string;
+		description: string;
+	}
+}
+
+enum Role {
+	ADMIN = "ADMIN",
+	READ_ONLY = 0,
+	AUTHOR = 100,
+}
+
+let answer: string | number;
+// answer = 452
+// answer = "452"
+// answer = false  /* Error: Type 'boolean' is not assignable to type 'string | number' */
+
+type Combinable = number | string;
+
+type User = { name: string; age: number };
+
+function greet(user: User) {
+	console.log("Hi, I am " + user.name);
+}
+
+function isOlder(user: User, checkAge: number) {
+	return checkAge > user.age;
+}
